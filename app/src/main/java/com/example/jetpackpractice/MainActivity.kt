a0953c87cd5fe21fde1d04e7904259b88e761cb8
shimanta505 <sarkershimanta@gmail.com>
2025-08-01 15:36:22 +0600

    imagecard praciced with brush

---
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.jetpackpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // enableEdgeToEdge()
+        setContent {
+        }
+    }
+}
+
+@Composable
+fun imageCard(){
+    val painter: Painter = painterResource(id = R.drawable.quotes)
+    val description: String = "kermit playing in the show"
+    val title:String = "he is playing very well"
+
+    Box(modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp)) {
+        Card(modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(15.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)){
+            Box(modifier = Modifier.height(200.dp).fillMaxWidth()){
+                Image(painter = painter,contentDescription = description,
+                    contentScale = ContentScale.FillWidth)
+
+                Box(modifier = Modifier.fillMaxSize().background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black),
+                        startY = 300f
+                    )
+                ))
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ){
+                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+            }
+        }
+    }
+
+}
