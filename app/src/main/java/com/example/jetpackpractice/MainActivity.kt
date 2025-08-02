4ea44bcafc50850426e61b48a46212d018e8e7f2
shimanta505 <sarkershimanta@gmail.com>
2025-08-02 18:32:48 +0600

    add counter state

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.jetpackpractice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -30,69 +40,48 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jetpackpractice.ui.theme.JetpackPracticeTheme
 import kotlin.random.Random
 import kotlin.random.nextInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
-            ColorBox(modifier = Modifier.fillMaxSize())
+            ShowPreview()
         }
     }
 }
 
 @Composable
-fun ColorBox(modifier: Modifier = Modifier){
-    val color = remember { mutableStateOf(Color.Yellow) }
-
-    Box(modifier = modifier.background(color = color.value).clickable{
-        val red = Random.nextInt(range = IntRange(0,255))
-        val green = Random.nextInt(range = IntRange(0,255))
-        val blue = Random.nextInt(range = IntRange(0,255))
-        color.value = Color(
-            red,
-            green,
-            blue,
-            0xFF
-        )
-        println("red || $red green || $green blue || $blue")
-    }){
-
+fun CounterState(){
+    val count = remember {
+        mutableIntStateOf(0)
     }
-}
 
-@Composable
-fun imageCard(){
-    val painter: Painter = painterResource(id = R.drawable.quotes)
-    val description: String = "kermit playing in the show"
-    val title:String = "he is playing very well"
-
-    Box(modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp)) {
-        Card(modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(15.dp),
-            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)){
-            Box(modifier = Modifier.height(200.dp).fillMaxWidth()){
-                Image(painter = painter,contentDescription = description,
-                    contentScale = ContentScale.FillWidth)
-
-                Box(modifier = Modifier.fillMaxSize().background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black),
-                        startY = 300f
-                    )
-                ))
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                    contentAlignment = Alignment.BottomStart
-                ){
-                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-                }
-            }
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+        Button(onClick = {
+            count.intValue++
+            Log.d("Count","${count.intValue}")
         }
+        ) {
+            Text("increase count")
+        }
+        Spacer(modifier = Modifier.size(25.dp))
+        Text("count value ${count.intValue}")
     }
-
 }
+
+@Preview
+@Composable
+fun ShowPreview(){
+    JetpackPracticeTheme {
+        CounterState()
+    }
+}
+
+
