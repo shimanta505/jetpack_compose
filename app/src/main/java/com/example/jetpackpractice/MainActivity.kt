53fcbd3d5d6fc2a5a4a0235a07de14a023195e4f
shimanta505 <sarkershimanta@gmail.com>
2025-08-02 18:36:02 +0600

    counter value changed to by keyword

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -26,9 +26,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -56,7 +58,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CounterState(){
-    val count = remember {
+    var count by remember {
         mutableIntStateOf(0)
     }
 
@@ -65,14 +67,14 @@ fun CounterState(){
         horizontalAlignment = Alignment.CenterHorizontally,
         ) {
         Button(onClick = {
-            count.intValue++
-            Log.d("Count","${count.intValue}")
+            count++
+            Log.d("Count","$count")
         }
         ) {
             Text("increase count")
         }
         Spacer(modifier = Modifier.size(25.dp))
-        Text("count value ${count.intValue}")
+        Text("count value $count")
     }
 }
 
