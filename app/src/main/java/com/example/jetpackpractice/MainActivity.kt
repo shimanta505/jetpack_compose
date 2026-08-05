77b81cd888c9031eef0b2943500d2c87905c9151
shimanta505 <sarkershimanta@gmail.com>
2026-08-05 15:30:28 +0600

    basic foreground notification

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.jetpackpractice
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -23,6 +26,7 @@ import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 
@@ -43,14 +47,40 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.jetpackpractice.ui.theme.JetpackPracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(this,
+                 arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0
+            )
+        }
         enableEdgeToEdge()
         setContent {
-            ShowPreview()
+            JetpackPracticeTheme {
+                Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also {
+                            it.action = RunningService.Actions.START.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text("Start run")
+                    }
+
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also {
+                            it.action = RunningService.Actions.STOP.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text("Stop run")
+                    }
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/jetpackpractice/RunningService.kt
@@ -0,0 +1,35 @@
+package com.example.jetpackpractice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class RunningService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action){
+            Actions.START.toString() -> start()
+            Actions.STOP.toString() -> stopSelf()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start(){
+        val notification = NotificationCompat.Builder(this,"running_channel")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Run is Active")
+            .setContentText("Elapsed Time")
+            .setContentText("Elapsed Time 00:50")
+            .build()
+
+        startForeground(1,notification)
+    }
+
+    enum class Actions{
+        START,STOP
+    }
+}
